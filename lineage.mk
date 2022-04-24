17193ad2454cc48d427ba057558706e12cb2f7c7
Alex <razielrazi3@gmail.com>
2022-04-24 17:09:11 +0300

    remove prop overrides

--- lineage.mk
+++ lineage.mk
@@ -11,13 +11,6 @@ PRODUCT_RELEASE_NAME := hllte
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Override build properties.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=hlltexx \
-    TARGET_DEVICE=hllte \
-    BUILD_FINGERPRINT="samsung/hlltexx/hllte:5.1.1/LMY47X/N7505POUDOK2:user/release-keys" \
-    PRIVATE_BUILD_DESC="hlltexx-user 5.1.1 LMY47X N7505POUDOK2 release-keys"
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hllte
 PRODUCT_NAME := lineage_hllte
