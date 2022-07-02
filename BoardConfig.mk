6b647f8a3462422d7210ad41268310625d4791ab
Alex <razielrazi3@gmail.com>
2022-07-02 13:13:25 +0300

    TWRP
    Change-Id: Id1252b93e051b0caa4b0f11c5409a27f7fb96ea9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,10 +2,10 @@
 -include vendor/samsung/smdk5260-common/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := hllte,hlltexx,hl3g
+TARGET_OTA_ASSERT_DEVICE := hllte,hlltexx,hl3g,hl3gxx
 
 # Kernel
-TARGET_KERNEL_CONFIG := hllte_defconfig
+TARGET_KERNEL_CONFIG := recovery-exynos5260_defconfig
 
 # RIL
 BOARD_MODEM_TYPE := xmm6360
