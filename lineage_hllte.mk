05c2e5de6358906bff3993ec4ca5320e9160ebe4
Alex <razielrazi3@gmail.com>
2022-07-05 11:18:58 +0300

    modernize hllte structure abit

--- lineage_hllte.mk
+++ lineage_hllte.mk
@@ -1,15 +1,25 @@
-# Inherit device configuration
-$(call inherit-product, device/samsung/hllte/full_hllte.mk)
+#ingerit device files
 $(call inherit-product, device/samsung/smdk5260-common/device-common.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 
+# Inherit from ha3g device
+$(call inherit-product, device/samsung/hllte/device.mk)
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/lineage/config/telephony.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := hllte
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# HLLTE vendor
+$(call inherit-product-if-exists, vendor/samsung/hllte/hllte-vendor.mk)
+
+# Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hllte
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo lineage_hllte-eng
-add_lunch_combo lineage_hllte-userdebug
